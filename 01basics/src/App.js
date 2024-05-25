54c1a227bc3e69be727005bf841876fcf682827a
neha0070 <jainnehal286@gmail.com>
2024-05-25 16:45:33 +0530

    Launced react projects

---
+++ 01basics/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1> Learning React | I am Nehal Jain, I have started learning react today. Today I have learned basics documentation about how I can make react project and about all instalation needed</h1>
+  );
+}
+
+export default App;
---
+++ ViteReact/src/App.jsx
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1> launching react project using Vite| Nehal Jain </h1>
+  )
+}
+
+export default App
