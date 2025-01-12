eba4f9c68fa0391c7936aaea28918edce7025b13
neha0070 <jainnehal286@gmail.com>
2025-01-12 21:04:45 +0530

    Removed unused files and added customReact directory

---
+++ customReact/customReact.js
@@ -0,0 +1,23 @@
+// underhood this work is happening .. 
+// this is our library how we want the users to use this functions
+
+function customRender(reactElement, container){
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+
+    container.appendChild(domElement)
+}
+const reactElement = {
+    type: 'a', // tag
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+const mainContainer = document.querySelector('#root')
+
+customRender(reactElement, mainContainer)
